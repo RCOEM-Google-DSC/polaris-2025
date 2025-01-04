a69355c721739a88d21ff5bb3f175bc4cfc9046c
annalhq <annalhqshaikh@gmail.com>
2025-01-04 18:09:56 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Polaris-25
+
+
+- [ ] Round 1
+
+- [ ] Round 2
+
+- [ ] Round 3
+
+- [ ] Round 4
---
+++ src/utils/gameUtils.ts
@@ -0,0 +1,23 @@
+export const TOTAL_TIME = 90; // 90 seconds per image
+export const INITIAL_POINTS = 1000;
+export const HINT_COSTS = [30, 50, 80];
+export const BLUR_LEVEL = 200; // Initial blur level
+export const BLUR_HEIGHT=Math.random() * 40 + 100
+export const BLUR_WIDTH=Math.random() * 40 + 100
+export const characters = [
+  { name: "Mario", image: "/ironman.jpeg" },
+];
+
+export function getRandomCharacters(n: number) {
+  const shuffled = [...characters].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, n);
+}
+
+export const COLORS = {
+  background: 'bg-gray-900',
+  text: 'text-gray-100',
+  primary: 'bg-purple-600',
+  secondary: 'bg-purple-800',
+  accent: 'text-purple-400',
+};
+
